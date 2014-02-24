99ea7111f4fec2c76c156f0686c4e0a212dd1ed5
Nick Burwell <nburwell@gmail.com>
2014-02-23 23:35:37 -0800

    Moved ruby files to lib directory

--- browser-glue.rb
+++ browser-glue.rb
@@ -4,7 +4,7 @@
 load File.expand_path('../secrets',  __FILE__)
 
 require 'bundler'
-require File.expand_path('../glue.rb',  __FILE__)
+require File.expand_path('../lib/glue.rb',  __FILE__)
 
 g = Glue.new
 g.issue_from_active_browser
