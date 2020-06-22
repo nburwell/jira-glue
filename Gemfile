c0da3e4709f022f3572dcfe6da79695f3addf963
Nick Burwell <nburwell@nbnburwell1.local>
2020-06-21 21:04:53 -0700

    Support OS X 10.15 Catalina by removing Pasteboard from Gemfile, upgrade ruby and rb-scpt; Clipboard support is now only for those that manually install Pasteboard gem

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source "http://rubygems.org"
 
-gem 'pasteboard'
-gem 'jira-ruby', '0.1.17', :require => 'jira'
+gem 'jira-ruby',         '0.1.17', require: 'jira'
 gem 'rb-scpt'
 gem 'terminal-notifier', '1.5.1'
--- lib/clipboard.rb
+++ lib/clipboard.rb
@@ -2,7 +2,12 @@
 #! /usr/bin/ruby
 
 require 'rubygems'
-require 'pasteboard'
+
+begin
+  require 'pasteboard'
+rescue LoadError
+  # this gem is not supported on Mac OS X 10.15+"
+end
 
 
 # Example usage
@@ -12,20 +17,39 @@ require 'pasteboard'
 
 class Clipboard
   def self.insert!(html, plain_text, slack_text)
-    pasteboard = Pasteboard.new
+    gem_uninitialized_wrapper("insert!") do
+      pasteboard = Pasteboard.new
 
-    item = [
-      [Pasteboard::Type::HTML,                   html],
-      [Pasteboard::Type::TEXT_MULTIMEDIA_DATA,   slack_text],
-      [Pasteboard::Type::PLAIN_TEXT_TRADITIONAL, plain_text]
-    ]
+      item = [
+        [Pasteboard::Type::HTML,                   html],
+        [Pasteboard::Type::TEXT_MULTIMEDIA_DATA,   slack_text],
+        [Pasteboard::Type::PLAIN_TEXT_TRADITIONAL, plain_text]
+      ]
 
-    pasteboard.put item
+      pasteboard.put item
+    end
   end
 
   def self.insert_text!(text)
-    pasteboard = Pasteboard.new
+    gem_uninitialized_wrapper("insert_text!") do
+      pasteboard = Pasteboard.new
+      pasteboard.put [[Pasteboard::Type::PLAIN_TEXT_TRADITIONAL, text]]
+    end
+  end
 
-    pasteboard.put [[Pasteboard::Type::PLAIN_TEXT_TRADITIONAL, text]]
+  def self.gem_uninitialized_wrapper method_name, &blk
+    begin
+      yield
+    rescue NameError => ex
+      if ex.message =~ /uninitialized constant/
+        message = <<~EOS
+          Pasteboard gem was not loaded. This gem is not supported on Mac OS X 10.15+ and so Clipboard::#{method_name} is not available.
+          On earlier OS X versions, be sure to 'gem install pasteboard' and try again.
+        EOS
+        raise message
+      else
+        raise ex
+      end
+    end
   end
 end
